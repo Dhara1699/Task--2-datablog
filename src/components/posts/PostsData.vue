425f27fa393a2d0365ab0fb88def3b8be23948c1
Dhara <dhara1.patel@bacancy.com>
2021-03-10 18:15:07 +0530

    commited!

--- src/components/posts/PostsData.vue
+++ src/components/posts/PostsData.vue
@@ -1,38 +1,73 @@
 <template>
-<header>
+  <header>
     <h2>PostsData</h2>
-</header>
-    <base-card>
-        <div>
-            <ul v-for="post in posts"  :key="post.userId">
-                <li id="item"><strong>UserId</strong> : {{post.userId}}</li>
-                <li><strong>Id</strong> : {{ post.id }}</li>
-                <li id="title"><strong>Title</strong> : {{ post.title }}</li>
-                <li><strong>Body</strong> : {{ post.body }}</li> 
-            </ul>
-        </div>
-    </base-card>
-
+  </header>
+  <base-card>
+    <div id="user">
+      <div id="posts">
+        <!-- <li v-for="user in users" :key="user.userId"> -->
+        <ul v-for="post in posts" :key="post.userId">
+            <a><strong>@{{post.user.username}}({{post.user.name}})</strong></a>
+          <!-- <li><strong>{{ user.username }}@{{ user.name }}</strong></li> -->
+          <!-- <li id="item"><strong>UserId</strong> : {{ post.userId }}</li> -->
+          <li id="item"><strong>Id</strong> : {{ post.id }}</li>
+          <li id="title"><strong>Title</strong> : {{ post.title }}</li>
+          <li><strong>Body</strong> : {{ post.body }}</li>
+        </ul>
+        <!-- </li> -->
+      </div>
+    </div>
+  </base-card>
 </template>
 
 <script>
-import axios from 'axios';
-window.axios = require('axios');
+import axios from "axios";
+window.axios = require("axios");
 export default {
-    data() {
-        return {
-            posts : null,
-        }; 
-    },
-    mounted: function() {
-        const headers = { "Content-Type": "application/json" };
-        axios.get('https://jsonplaceholder.typicode.com/posts' , { headers }).then(response =>
-        {
-            console.log(response.data);
-            this.posts=response.data;
-        }) 
-    }
-}   
+  data() {
+    return {
+      posts: [],
+      users: [],
+    };
+  },
+  mounted() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+        this.users = response.data;
+
+        axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => {
+
+            response.data.map((post) => {
+                post.user = this.users.find((user) => user.id === post.userId)
+            })
+            this.posts = response.data;
+        })
+    });
+
+    // const headers = { "Content-Type": "application/json" };
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/posts", { headers })
+    //   .then((response) => {
+    //     console.log(response.data);
+    //     this.posts = response.data;
+    //     /*for(let key in this.posts){
+    //             axios.get(`https://jsonplaceholder.typicode.com/users/${this.posts[key].userId}`).then(res =>
+    //             {
+    //                 console.log(res.data);
+
+    //                 //this.users = response.data;
+    //             }) 
+    //         }*/
+    //     axios
+    //       .get("https://jsonplaceholder.typicode.com/users")
+    //       .then((response) => {
+    //         console.log(response.data);
+    //         return (this.users = response.data);
+    //       });
+    //   });
+  },
+};
 </script>
 
 <style scoped>
@@ -41,7 +76,7 @@ export default {
 }
 
 html {
-  font-family: 'Jost', sans-serif;
+  font-family: "Jost", sans-serif;
 }
 
 body {
@@ -60,22 +95,26 @@ header {
   max-width: 40rem;
 }
 
- .ul {
+.ul {
   margin: 0;
   padding: 0;
   list-style: none;
 }
 
- li {
+li {
   list-style-type: none;
   text-align: center;
 }
 
-#title{
-    color: blue;
+#title {
+  color: blue;
+  font-size: larger;
+  font-family: Georgia, "Times New Roman", Times, serif;
+  text-decoration: underline;
+  font-weight: bold;
 }
 #item {
-    text-decoration-style: inherit;
-    color: red;
+  text-decoration-style: inherit;
+  color: red;
 }
 </style>
